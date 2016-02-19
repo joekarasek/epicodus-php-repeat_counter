111858b9d85de33a72a1d92abd41700acd6541eb
Joseph Karasek <joseph.karasek@gmail.com>
2016-02-19 08:10:57 -0800

    add 6 specs to RepeatCounterTest.php that match English specs in tests.md

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -1,15 +1,85 @@
 <?php
-    require_once __DIR__ . '/../src/CountRepeat.php';
+    require_once __DIR__ . '/../src/RepeatCounter.php';
 
     class ClassTest extends PHPUnit_Framework_TestCase
     {
-        function test_methodToTest_inputDescription()
+        function test_countRepeat_oneWordNoMatch()
         {
             // Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $phrase = 'cat';
+            $word_to_count = 'dog';
 
             // Act
+            $result = $test_RepeatCounter->CountRepeat($phrase, $word_to_count);
 
             // Assert
+            $this->assertEquals( 0 , $result);
+        }
+        function test_countRepeat_oneWordOneMatch()
+        {
+            // Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $phrase = 'cat';
+            $word_to_count = 'cat';
+
+            // Act
+            $result = $test_RepeatCounter->CountRepeat($phrase, $word_to_count);
+
+            // Assert
+            $this->assertEquals( 1 , $result);
+        }
+        function test_countRepeat_simpleSentence()
+        {
+            // Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $phrase = 'This cat is a special cat';
+            $word_to_count = 'cat';
+
+            // Act
+            $result = $test_RepeatCounter->CountRepeat($phrase, $word_to_count);
+
+            // Assert
+            $this->assertEquals( 2 , $result);
+        }
+        function test_countRepeat_simpleSentenceCaseInsensitive()
+        {
+            // Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $phrase = 'Cat food is f-ing expensive!';
+            $word_to_count = 'cat';
+
+            // Act
+            $result = $test_RepeatCounter->CountRepeat($phrase, $word_to_count);
+
+            // Assert
+            $this->assertEquals( 1 , $result);
+        }
+        function test_countRepeat_specialCharacters()
+        {
+            // Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $phrase = 'When i say "cat", you say "Cat!".';
+            $word_to_count = 'cat';
+
+            // Act
+            $result = $test_RepeatCounter->CountRepeat($phrase, $word_to_count);
+
+            // Assert
+            $this->assertEquals( 2 , $result);
+        }
+        function test_countRepeat_noPartialMatches()
+        {
+            // Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $phrase = 'There are four cats in the cathedral, but none of them are my cat.';
+            $word_to_count = 'cat';
+
+            // Act
+            $result = $test_RepeatCounter->CountRepeat($phrase, $word_to_count);
+
+            // Assert
+            $this->assertEquals( 1 , $result);
         }
     }
  ?>
