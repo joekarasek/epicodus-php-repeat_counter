2a02d7ce2bbf598a773536811e7c8dc685681f00
Joseph Karasek <joseph.karasek@gmail.com>
2016-02-19 07:59:06 -0800

    add files for class CountRepeat and its specs

---
+++ tests/CountRepeatTest.php
@@ -0,0 +1,15 @@
+<?php
+    require_once __DIR__ . '/../src/CountRepeat.php';
+
+    class ClassTest extends PHPUnit_Framework_TestCase
+    {
+        function test_methodToTest_inputDescription()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+ ?>
