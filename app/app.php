bd5677496fe39615c324a361bc30cbd6fc6319fc
Joseph Karasek <joseph.karasek@gmail.com>
2016-02-19 08:44:55 -0800

    add UI to take input and return results

--- app/app.php
+++ app/app.php
@@ -1,5 +1,6 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -8,7 +9,20 @@
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig', array(
-            // twig input associative array
+            'form' => true
+        ));
+    });
+
+    $app->get("/result", function() use ($app) {
+        $my_RepeatCounter = new RepeatCounter;
+        $result = $my_RepeatCounter->CountRepeat($_GET['phrase'], $_GET['word_to_count']);
+
+        return $app['twig']->render('index.html.twig', array(
+            'form' => true,
+            'message' => array(
+                'type' => 'info',
+                'text' => 'The method CountRepeat found ' . $result . ' instances of the word "' . $_GET['word_to_count'] . '" in the phrase you provided.'
+            )
         ));
     });
 
